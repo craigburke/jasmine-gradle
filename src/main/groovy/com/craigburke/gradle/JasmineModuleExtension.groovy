74db5745dd0899ae452bbdb12af8ccb74d5dff09
Craig Burke <craig@craigburke.com>
2015-07-25 16:09:39 -0400

    Initial commit

---
+++ src/main/groovy/com/craigburke/gradle/JasmineModuleExtension.groovy
@@ -0,0 +1,29 @@
+package com.craigburke.gradle
+
+import groovy.json.JsonBuilder
+
+class JasmineModuleExtension {
+    
+    List dependencies = ['karma', 'karma-jasmine@2_0', 'karma-phantomjs-launcher', 'phantomjs']
+
+    String basePath = ''
+    List frameworks = ['jasmine']
+    List browsers = ['PhantomJS']
+    List files = []
+    List excludes = []
+    Map additional = [:]
+    
+    String getConfigJavaScript() {
+        Map properties = [basePath: basePath, files: files, browsers: browsers,
+                          frameworks: frameworks, excludes: excludes ]
+
+        additional.each{ properties[it.key] = it.value }
+
+        def json = new JsonBuilder()
+        json(properties)
+        String configJson = json.toString()
+        
+        "module.exports = function(config) { config.set(${configJson}) };"
+    }
+}
+
---
+++ src/main/groovy/com/craigburke/gradle/JasminePlugin.groovy
@@ -0,0 +1,70 @@
+package com.craigburke.gradle
+
+import com.moowork.gradle.node.task.NodeTask
+import com.moowork.gradle.node.task.NpmTask
+import org.gradle.api.GradleException
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.api.Task
+import org.gradle.api.tasks.Delete
+
+class JasminePlugin implements Plugin<Project> {
+
+    void apply(Project project) {
+        
+        def nodeConfig = project.extensions.findByName('node')
+        if (!nodeConfig) {
+            throw new GradleException('The Node plugin needs to be installed and applied.')
+        }
+
+        final String NPM_OUTPUT_PATH = project.file(nodeConfig.nodeModulesDir).absolutePath + '/node_modules/'
+        final File KARMA_EXEC = project.file(NPM_OUTPUT_PATH + '/karma/bin/karma')
+        final File KARMA_CONFIG = project.file("${project.buildDir.absolutePath}/karma.conf.js")
+        
+        def jasmineConfig = project.extensions.create('jasmine', JasmineModuleExtension)
+
+        boolean grailsPluginApplied = project.extensions.findByName('grails')
+        jasmineConfig.basePath = grailsPluginApplied ? '../grails-app/assets/libs/' : '../src/assets/'
+
+        Task jasmineDependencies = project.task('jasmineDependencies',
+                type: NpmTask,
+                group: 'Jasmine',
+                description: 'Installs dependencies needed for running Jasmine tests.')
+
+        jasmineDependencies.configure {
+            args = ['install'] + jasmineConfig.dependencies + ['--silent']
+            outputs.files jasmineConfig.dependencies.collect { "${NPM_OUTPUT_PATH}/${it.split('@')[0]}" } + KARMA_CONFIG
+        }
+        jasmineDependencies.doLast {
+            KARMA_CONFIG.parentFile.mkdirs()
+            KARMA_CONFIG.text = jasmineConfig.configJavaScript
+        }
+
+        Task jasmineRun = project.task('jasmineRun',
+                 type: NodeTask, dependsOn: 'jasmineDependencies', group: 'Jasmine',
+                 description: 'Executes jasmine tests')
+        
+        jasmineRun.configure {
+            script = KARMA_EXEC
+            args = ['start', KARMA_CONFIG.absolutePath, '--single-run']
+        }
+
+        Task jasmineWatch = project.task('jasmineWatch',
+                type: NodeTask, dependsOn: 'jasmineDependencies', group: 'Jasmine',
+                description: 'Executes jasmine tests in watch mode')
+
+        jasmineWatch.configure {
+            script = KARMA_EXEC
+            args = ['start', KARMA_CONFIG.absolutePath, '--auto-watch']
+        }
+
+        Task jasmineClean = project.task('jasmineClean',
+                type: Delete, group: 'Jasmine',
+                description: 'Deletes the generated karma config file')
+        jasmineClean.configure {
+            delete KARMA_CONFIG
+        }
+
+    }
+
+}
